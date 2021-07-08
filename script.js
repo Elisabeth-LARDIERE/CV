4e14613c3ae864262a9e5fc89302a3549669e029
Elisabeth Lardiere <babeth.lardiere@gmail.com>
2021-07-08 10:26:48 +0200

    correction scroll retour haut de page

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ $(function () {
 
         event.preventDefault();
         let hash = this.hash;
-        console.log(hash);
 
         $('body,html').animate({scrollTop: $(hash).offset().top}, 900, function () {
             window.location.hash = hash;
